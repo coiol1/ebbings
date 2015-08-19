6b0231a4a6c53212fed1bc56330f3181833bcc29
Glenn M. Davis <gmdavis1@gmail.com>
2015-08-19 11:05:10 -0600

    heroku push

--- ebbings/settings.py
+++ ebbings/settings.py
@@ -112,5 +112,27 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
+STATIC_ROOT = 'staticfiles'
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+if os.environ.has_key('DATABASE_URL') and dj_database_url.config():
+    DATABASES['default'] = dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
